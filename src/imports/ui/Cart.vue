e9dc68f8b7add634c1e35129d7a3550a87e845ad
alexycruz1 <alexyc@unitec.edu>
2018-04-09 22:27:28 -0600

    Log In page, register modal, exit app icon

--- src/imports/ui/Cart.vue
+++ src/imports/ui/Cart.vue
@@ -4,8 +4,18 @@
       <div class="backdrop" @click="close" />
       <div class="panel" :class="$style.cart">
         <div class="empty-text" :class="$style.meow">
-          Your cart is <span :class="foo.bar">empty</span>
+          <h1>Sign Up</h1>
+          <h4>Enter the following information in order to create a account in our organization</h4>
+          <div>
+            <input type="email" name="usremail" placeholder="myEmail@something.com">
           </div>
+          <div>
+            <input type="password" name="pwd" placeholder="Type your password">
+          </div>
+          <div>
+            <button class="btn" type="button">Sign In</button>  
+          </div>
+        </div>
       </div>
     </div>
   </transition>
@@ -36,6 +46,28 @@ export default {
 
 .cart-overlay {
   .overlay;
+}
+
+.btn {
+  width: 150px;
+  height: 40px;
+  margin-right: 20px;
+  padding: 0;
+  margin-top:5px;
+
+  border-width: 0;
+  outline: none;
+  border-radius: 2px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, .6);
+  
+  background-color: #374046;
+  color: #ecf0f1;
+  
+  transition: background-color .3s;
+}
+
+.btn:hover, .btn:focus {
+  background-color: rgb(158, 158, 158);
 }
 </style>
 
---
+++ src/imports/ui/LoginRegister.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class = "container">
+
+    <div class = "InformationContainer">
+      <div>
+        <img src = "http://www.elhogar.org/main/wp-content/uploads/2013/01/elhogar-logo-site.png">
+      </div>
+
+      <h1>A community of lifelong learners, responsible global citizens, and champions of our own success.</h1>
+
+      <h4>El Hogar is a comprehensive four-year public high school enrolling 1,250 students in grades 9–12. 
+      The school opened in the fall of 1987 and graduated its first senior class in the spring of 1989.</h4>
+
+      <input type="email" name="usremail">
+      <input type="password" name="pwd">
+      <button onclick="window.location.href='/events'" class="btn" type="button">Sign In</button>
+      <h2 @click="openCart"><a>Create account</a></h2>
+      
+    </div>
+
+    <div class = "Video">
+      <img src = "https://lh3.googleusercontent.com/oKyiLTuQGXuPVIoaIYoVFutapnCAsGaMeBArFXGNlFEbNP3O4g7QKGgu1G7nvzSNEHKI=h310">
+    </div>
+
+  </div>
+</template>
+
+<style lang = "less" scoped>
+
+.container{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+}
+
+.Video{
+  margin-left: 10%;
+  margin-top: 8%;
+}
+
+.InformationContainer{
+  margin-left:2%;
+  margin-top:2%;
+  width: 50%;
+  height: 92%;
+}
+
+.btn {
+  width: 150px;
+  height: 40px;
+  margin-right: 20px;
+  padding: 0;
+
+  border-width: 0;
+  outline: none;
+  border-radius: 2px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, .6);
+  
+  background-color: #374046;
+  color: #ecf0f1;
+  
+  transition: background-color .3s;
+}
+
+.btn:hover, .btn:focus {
+  background-color: rgb(158, 158, 158);
+}
+
+</style>
+
+<script>
+  import { mapActions } from 'vuex'
+  
+  export default {
+    methods: {
+      ...mapActions('layout', [
+        'showCart'
+      ]),
+      openCart () {
+        this.showCart(true)
+      },
+    },
+  }
+</script>
